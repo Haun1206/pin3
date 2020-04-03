e1f375cb7abbe3c3503a3eb58e12dcc239fe2310
Jio <harryoh99@naver.com>
2020-04-04 03:26:32 +0900

    donation

--- threads/thread.c
+++ threads/thread.c
@@ -733,7 +733,7 @@ void refresh_priority(void){
 void donate_priority(void){
     int depth = 0;
     struct thread * cur = thread_current();
-    struct lock * temp_lock = cur->want_lock();
+    struct lock * temp_lock = cur->want_lock;
     while(temp_lock != NULL && depth < DEPTH_MAX){
         depth++;
         //No lock holder
