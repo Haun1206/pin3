574612603ae0f252073e4fd377a0b30bc3e6763a
Jio <harryoh99@naver.com>
2020-04-05 01:10:17 +0900

    mlfqs

--- threads/thread.c
+++ threads/thread.c
@@ -457,7 +457,7 @@ thread_set_nice (int nice ) {
     intr_enable();
     mlfqs_recent_cpu(thread_current());
     mlfqs_priority(thread_current());
-    intr_yield_on_return();
+    swap_working();
 }
 
 /* Returns the current thread's nice value. */
