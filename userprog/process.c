fc5641064b08b583f6456b91a9b457827291c244
jiooh_windows <harryoh99@gmail.com>
2020-04-29 22:58:11 +0900

    tempo

--- userprog/process.c
+++ userprog/process.c
@@ -28,6 +28,8 @@ static bool load (const char *file_name, struct intr_frame *if_);
 static void initd (void *f_name);
 static void __do_fork (void *);
 static void argument_stack(char * parse[], int count, struct intr_frame *if_);
+struct thread * get_child_process(int pid);
+void remove_child_process(struct thread *cp);
 int process_add_file(struct file *f);
 struct file *process_get_file(int fd);
 void process_close_file(int fd);
