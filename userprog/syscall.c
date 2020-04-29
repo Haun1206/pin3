32901e948175222b2797678d2aa985d7b0ebb3b6
jiooh_windows <harryoh99@gmail.com>
2020-04-29 20:32:21 +0900

    tempo

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -10,6 +10,20 @@
 
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
+void halt (void);
+void exit (int status);
+pid_t fork(const char *thread_name);
+int exec (const char *cmd_line);
+int wait (pid_t pid);
+bool create(const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize(int fd);
+int read(int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close(int fd);
 
 /* System call.
  *
@@ -24,6 +38,7 @@ void syscall_handler (struct intr_frame *);
 #define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
+struct lock file_lock;	
 
 static void check_address(void* addr){
 	/*if minimum needed check addr>(void)0x0*/
