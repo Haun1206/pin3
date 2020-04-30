e21713bc58d48b2ec47cf6b8153cb80d04aebcc4
jiooh_windows <harryoh99@gmail.com>
2020-05-01 03:22:05 +0900

    tempo

--- userprog/process.c
+++ userprog/process.c
@@ -274,8 +274,10 @@ process_exit (void) {
 	 * TODO: Implement process termination message (see
 	 * TODO: project2/process_termination.html).
 	 * TODO: We recommend you to implement process resource cleanup here. */
-	for(int i=3;i<curr->next_fd;i++)
+	for(int i=2;i<curr->next_fd;i++)
 		process_close_file(i);
+		
+	printf("%s\n", "Is this working?");
 	palloc_free_page(curr->fd_table);
 	/*close the currently running file*/
 	curr->process_exit = true;
