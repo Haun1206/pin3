a0708e81c4bd9850fcf36b6e7f0ea762e00e968a
jiooh_windows <harryoh99@gmail.com>
2020-05-01 01:50:08 +0900

    tempo

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -231,7 +231,9 @@ void
 syscall_handler (struct intr_frame *f UNUSED) {
 	// TODO: Your implementation goes here.
 	int64_t args[6];
+	printf("%s\n", "maybe?");
 	check_addr(&f->rsp);
+	printf("%s\n", "maybe?");
 	switch(f->R.rax){
 		case SYS_HALT:
 			halt();
