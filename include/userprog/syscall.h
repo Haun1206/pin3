6c02dce11065c052f7b48d2b2d8049846de13fde
jiooh_windows <harryoh99@gmail.com>
2020-05-02 17:51:32 +0900

    50 TMP

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -1,39 +1,11 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-#include "userprog/syscall.h"
-#include <stdio.h>
-#include <syscall-nr.h>
-#include "filesys/filesys.h"
-#include "filesys/file.h"
-#include "threads/interrupt.h"
-#include "threads/thread.h"
-#include "threads/loader.h"
-#include "threads/palloc.h"
-#include "userprog/gdt.h"
-#include "threads/flags.h"
-#include "intrinsic.h"
+
 void syscall_init (void);
-static void check_addr(void* addr);
-/*void get_argument(struct intr_frame * f, int * arg, int count);*/
-void check_str(void * str);
-void check_buf(void *buffer, unsigned size);
-void syscall_entry (void);
-void syscall_handler (struct intr_frame *);
-void halt (void);
+
 void exit (int status);
-int fork(const char *thread_name,struct intr_frame *f);
-int exec (const char *cmd_line);
-int wait (int pid);
-bool create(const char *file, unsigned initial_size);
-bool remove (const char *file);
-int open (const char *file);
-int filesize(int fd);
-int read(int fd, void *buffer, unsigned size);
-int write (int fd, const void *buffer, unsigned size);
-void seek (int fd, unsigned position);
-unsigned tell (int fd);
-void close(int fd);
+
 
 struct lock file_lock;
 
