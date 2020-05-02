f56c7cf966be3d1e72cf0d20281d7a7f4c225aa5
jiooh_windows <harryoh99@gmail.com>
2020-05-03 03:59:54 +0900

    plz

--- userprog/process.c
+++ userprog/process.c
@@ -268,7 +268,6 @@ process_wait (tid_t child_tid UNUSED) {
 	int res_status;
 	struct thread* child = get_child_process((int)child_tid);
 	if(child ==NULL || child->child_status_exit==-1){
-		list_remove(&child->child_elem);
 		return -1;
 	}
 		
