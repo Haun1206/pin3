83c67b659fdcef90dcc4ef729e77ebdffc4c7417
jiooh_windows <harryoh99@gmail.com>
2020-05-02 22:31:34 +0900

    zz

--- threads/thread.c
+++ threads/thread.c
@@ -13,11 +13,14 @@
 #include "intrinsic.h"
 #include "threads/fixed_point.h"
 #include <list.h>
-
+#include "userprog/process.h"
+#include "userprog/syscall.h
+/*
 #ifdef USERPROG
 #include "userprog/process.h"
 #include "userprog/syscall.h"
 #endif
+*/
 
 /* Random value for struct thread's `magic' member.
    Used to detect stack overflow.  See the big comment at the top
