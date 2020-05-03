d45ba902c6292415f0eb0292948ff7f7569bc551
jiooh_windows <harryoh99@gmail.com>
2020-05-04 02:26:34 +0900

    plz

--- userprog/process.c
+++ userprog/process.c
@@ -98,8 +98,10 @@ process_fork (const char *name, struct intr_frame *if_ UNUSED) {
 	t->forked =1;
 	tid_t id = thread_create(name, PRI_DEFAULT, __do_fork, if_);
 	printf("FORKED NEW ONE ID: %d",id);
-	if(t->child_status_exit ==TID_ERROR)
+	if(t->child_status_exit ==TID_ERROR){
 		id = TID_ERROR;
+		printf("IT HAS THIS ERROR\n");
+	}
 	sema_down(&t->child_fork);
 	return id;
 }
