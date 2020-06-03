b97410e55dbdecffd7aad416a777d5b94404cc8f
jiooh_windows <harryoh99@gmail.com>
2020-06-04 04:00:18 +0900

    printf check

--- userprog/process.c
+++ userprog/process.c
@@ -966,7 +966,7 @@ setup_stack (struct intr_frame *if_) {
 	 * TODO: If success, set the rsp accordingly.
 	 * TODO: You should mark the page is stack. */
 	/* TODO: Your code goes here */
-    if(vm_alloc_page(VM_MARKER_0 | VM_ANON, stack_bottom, false)&& vm_claim_page(stack_bottom)){
+    if(vm_alloc_page_with_initializer(VM_ANON, stack_bottom, true, NULL, NULL)){
         if_->rsp = USER_STACK;
         success = true;
     }
