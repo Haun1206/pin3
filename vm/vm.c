d675a9e2283aec54ad3e02d5039078709fc833b3
jiooh_windows <harryoh99@gmail.com>
2020-06-06 02:15:43 +0900

    printf check

--- vm/vm.c
+++ vm/vm.c
@@ -95,7 +95,7 @@ struct page *
 spt_find_page (struct supplemental_page_table *spt UNUSED, void *va UNUSED) {
 	struct page p;
     p.va = va;
-    struct hash_elem * elem= hash_find(&spt->hash_table, &page->h_elem);
+    struct hash_elem * elem= hash_find(&spt->hash_table, &p.h_elem);
     free(page);
     if (elem==NULL)
         return NULL;
