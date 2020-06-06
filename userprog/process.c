5cf9b1cda5fe183a97a049103d78f88109eb2118
jiooh_windows <harryoh99@gmail.com>
2020-06-06 22:07:21 +0900

    p

--- userprog/process.c
+++ userprog/process.c
@@ -596,7 +596,7 @@ load (const char *file_name, struct intr_frame *if_) {
 done:
 	/* We arrive here whether the load is successful or not. */
     //printf("%d\n",4);
-	file_close (file);
+	//file_close (file);
 	//free(arguments);
 	
 	return success;
