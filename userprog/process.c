25d4d37dc473d65a5bb4310a0f0b1939e7e263ca
jiooh_windows <harryoh99@gmail.com>
2020-06-07 21:15:35 +0900

    p

--- userprog/process.c
+++ userprog/process.c
@@ -573,7 +573,7 @@ load (const char *file_name, struct intr_frame *if_) {
 					}
 					if (!load_segment (file, file_page, (void *) mem_page,
 								read_bytes, zero_bytes, writable)){
-						printf("HERE3\n");
+						//printf("HERE3\n");
 						goto done;
 					}
 				}
@@ -876,7 +876,7 @@ lazy_load_segment (struct page *page, void *aux) {
 	/* TODO: VA is available when calling this function. */
     
     struct aux_load * aux_t = (struct aux_load *)aux;
-    
+    file_seek(aux_t->file, aux_t->ofs);
     //In aux it has file, ofs, read_bytes, zero_bytes, writable
     //Should modify this part
     /* Get a page of memory. */
@@ -884,7 +884,7 @@ lazy_load_segment (struct page *page, void *aux) {
         return false;
     else{
         uint8_t * kva = page->frame->kva;
-        if (file_read_at(aux_t->file, kva, aux_t->read_bytes, aux_t->ofs) != (int) aux_t->read_bytes) {
+        if (file_read(aux_t->file, kva, aux_t->read_bytes) != (int) aux_t->read_bytes) {
             printf("SOMETHING IS WRONG\n");
             return false;
         }
