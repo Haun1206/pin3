eda9fac9c495c86714da4b7b59a603710e264d96
jiooh_windows <harryoh99@gmail.com>
2020-06-07 21:17:06 +0900

    p

--- userprog/process.c
+++ userprog/process.c
@@ -876,7 +876,7 @@ lazy_load_segment (struct page *page, void *aux) {
 	/* TODO: VA is available when calling this function. */
     
     struct aux_load * aux_t = (struct aux_load *)aux;
-    file_seek(aux_t->file, aux_t->ofs);
+    
     //In aux it has file, ofs, read_bytes, zero_bytes, writable
     //Should modify this part
     /* Get a page of memory. */
@@ -884,7 +884,7 @@ lazy_load_segment (struct page *page, void *aux) {
         return false;
     else{
         uint8_t * kva = page->frame->kva;
-        if (file_read(aux_t->file, kva, aux_t->read_bytes) != (int) aux_t->read_bytes) {
+        if (file_read_at(aux_t->file, kva, aux_t->read_bytes, aux_t->ofs) != (int) aux_t->read_bytes) {
             printf("SOMETHING IS WRONG\n");
             return false;
         }
@@ -969,7 +969,7 @@ setup_stack (struct intr_frame *if_) {
     if(vm_alloc_page(VM_MARKER_0 | VM_ANON, stack_bottom, true)){
 		if(vm_claim_page(stack_bottom)){
 		
-        	if_->rsp = stack_bottom+PGSIZE;
+        	if_->rsp = USER_STACK;
         	success = true;
    		}
 	}
