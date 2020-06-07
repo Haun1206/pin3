1346bf736623881a10a15ae2ca09a385a8a116f6
jiooh_windows <harryoh99@gmail.com>
2020-06-07 22:44:52 +0900

    p

--- userprog/process.c
+++ userprog/process.c
@@ -168,8 +168,10 @@ __do_fork (void *aux) {
 	process_activate (current);
 #ifdef VM
 	supplemental_page_table_init (&current->spt);
-	if (!supplemental_page_table_copy (&current->spt, &parent->spt))
+	if (!supplemental_page_table_copy (&current->spt, &parent->spt)){
+		printf("IS THIS THE WRONG ONE?\n");
 		goto error;
+	}
 #else
 	if (!pml4_for_each (parent->pml4, duplicate_pte, parent))
 		goto error;
